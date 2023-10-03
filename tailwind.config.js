7d34bffe2df6af1618fd84934582540ebd71b73c
farha-abdullah <farhan.abdullah94@gmail.com>
2023-10-03 09:54:40 +0200

    added custom colors and banner picture

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,11 @@ export default {
 	content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
 	theme: {
 		extend: {},
+		colors: {
+			primary: '#42b8d4',
+			secondary: '#337ab7',
+			fourth: '#858585',
+		},
 	},
 	plugins: [],
 };
