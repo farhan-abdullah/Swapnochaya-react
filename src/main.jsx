090a36d2bd6e4f763d742b55cf2fe55e24968b0e
farhan-abdullah <farhan.abdullah94@gmail.com>
2023-10-03 20:05:25 +0200

    created project structure

--- src/main.jsx
+++ src/main.jsx
@@ -2,11 +2,19 @@ import * as React from 'react';
 import * as ReactDOM from 'react-dom/client';
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import './index.css';
+import HomeLayout from './Layout/HomeLayout';
+import Home from './components/Home/Home';
 
 const router = createBrowserRouter([
 	{
 		path: '/',
-		element: <div>Hello world!</div>,
+		element: <HomeLayout></HomeLayout>,
+		childred: [
+			{
+				path: '/',
+				element: <Home></Home>,
+			},
+		],
 	},
 ]);
 
