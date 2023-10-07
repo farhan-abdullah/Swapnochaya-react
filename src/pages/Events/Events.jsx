7e6a3340fe48f7ef5b05f55c44416f6c5317b334
monsur776e <monsuralam8897@gmail.com>
2023-10-07 19:34:13 +0600

    event right section complete

--- src/pages/Events/Events.jsx
+++ src/pages/Events/Events.jsx
@@ -4,7 +4,7 @@ const Events = () => {
    return (
       <>
          {/* // completed task */}
-         <div className="bg-white my-[100px]">
+         <div className="bg-white mt-[100px]">
             <div className="finished-project-container  grid grid-cols-2 md:grid-cols-4 mx-auto">
                <div className="flex flex-col space-y-2 h-[125px] border border-gray-500/40 justify-center items-center">
                   <p className="font-sans text-[12px] uppercase text-[#0000004d]">
@@ -33,8 +33,123 @@ const Events = () => {
             </div>
          </div>
          {/* // upcoming events */}
-         <div className="grid md:grid-cols-2 ">
-            <div className="bg-white"></div>
+         <div className="grid md:grid-cols-2  ">
+            <div className="bg-white pl-[60px]">
+               <div className="md:mb-[40px]">
+                  <h2 className="text-[36px] font-semibold pb-2">
+                     Upcoming events
+                  </h2>
+                  <div className="w-[80px] h-[2px] bg-primary"></div>
+               </div>
+               {/* end the title */}
+
+               <div className="event-container">
+                  <div className="flex border-b-[1px] my-5 border-gray-300/70 pb-3 gap-x-7 justify-center items-center ">
+                     <div className="  bg-primary px-4 py-1">
+                        <h1 className="text-center text-[34px] text-[#ffffff]">
+                           14
+                        </h1>
+                        <hr />
+                        <h2 className="text-[#ffffff] uppercase mt-1 text-center text-[15px]">
+                           JAN
+                           <br />
+                           2016
+                        </h2>
+                     </div>
+                     <div>
+                        <span className="text-[12px] text-[#0000004d]">
+                           Thursday, 11:20 AM
+                        </span>
+                        <h3 className="text-[18px] text-[#333] font-semibold py-1">
+                           Summer Camp: Student get together
+                        </h3>
+                        <p className="text-[15px] text-[#898989]">
+                           Lorem ipsum dolor sit amet, consectet adipiscing elit
+                           Cras sed nunc massa. Quisque tempor dolor sit amet
+                           tellus malesuada...
+                        </p>
+                     </div>
+                  </div>
+                  <div className="flex border-b-[1px] my-5 border-gray-300/70 pb-3 gap-x-7 justify-center items-center ">
+                     <div className="  bg-primary px-4 py-1">
+                        <h1 className="text-center text-[34px] text-[#ffffff]">
+                           14
+                        </h1>
+                        <hr />
+                        <h2 className="text-[#ffffff] uppercase mt-1 text-center text-[15px]">
+                           JAN
+                           <br />
+                           2016
+                        </h2>
+                     </div>
+                     <div>
+                        <span className="text-[12px] text-[#0000004d]">
+                           Thursday, 11:20 AM
+                        </span>
+                        <h3 className="text-[18px] text-[#333] font-semibold py-1">
+                           Summer Camp: Student get together
+                        </h3>
+                        <p className="text-[15px] text-[#898989]">
+                           Lorem ipsum dolor sit amet, consectet adipiscing elit
+                           Cras sed nunc massa. Quisque tempor dolor sit amet
+                           tellus malesuada...
+                        </p>
+                     </div>
+                  </div>
+                  <div className="flex border-b-[1px] my-5 border-gray-300/70 pb-3 gap-x-7 justify-center items-center ">
+                     <div className="  bg-primary px-4 py-1">
+                        <h1 className="text-center text-[34px] text-[#ffffff]">
+                           14
+                        </h1>
+                        <hr />
+                        <h2 className="text-[#ffffff] uppercase mt-1 text-center text-[15px]">
+                           JAN
+                           <br />
+                           2016
+                        </h2>
+                     </div>
+                     <div>
+                        <span className="text-[12px] text-[#0000004d]">
+                           Thursday, 11:20 AM
+                        </span>
+                        <h3 className="text-[18px] text-[#333] font-semibold py-1">
+                           Summer Camp: Student get together
+                        </h3>
+                        <p className="text-[15px] text-[#898989]">
+                           Lorem ipsum dolor sit amet, consectet adipiscing elit
+                           Cras sed nunc massa. Quisque tempor dolor sit amet
+                           tellus malesuada...
+                        </p>
+                     </div>
+                  </div>
+                  <div className="flex border-b-[1px] my-5 border-gray-300/70 pb-3 gap-x-7 justify-center items-center ">
+                     <div className="  bg-primary px-4 py-1">
+                        <h1 className="text-center text-[34px] text-[#ffffff]">
+                           14
+                        </h1>
+                        <hr />
+                        <h2 className="text-[#ffffff] uppercase mt-1 text-center text-[15px]">
+                           JAN
+                           <br />
+                           2016
+                        </h2>
+                     </div>
+                     <div>
+                        <span className="text-[12px] text-[#0000004d]">
+                           Thursday, 11:20 AM
+                        </span>
+                        <h3 className="text-[18px] text-[#333] font-semibold py-1">
+                           Summer Camp: Student get together
+                        </h3>
+                        <p className="text-[15px] text-[#898989]">
+                           Lorem ipsum dolor sit amet, consectet adipiscing elit
+                           Cras sed nunc massa. Quisque tempor dolor sit amet
+                           tellus malesuada...
+                        </p>
+                     </div>
+                  </div>
+               </div>
+            </div>
             <div className="bg-primary"></div>
          </div>
       </>
