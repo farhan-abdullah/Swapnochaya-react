6ba1071aa7cf6eae2127efd7f152f288037e1d1c
farhan-abdullah <farhan.abdullah94@gmail.com>
2023-10-08 13:54:37 +0200

    added 404 page

--- src/main.jsx
+++ src/main.jsx
@@ -1,26 +1,31 @@
-import * as React from "react";
-import * as ReactDOM from "react-dom/client";
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
-import "./index.css";
-import HomeLayout from "./Layout/HomeLayout";
-import Home from "./pages/Homes/Home/Home";
+import * as React from 'react';
+import * as ReactDOM from 'react-dom/client';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import './index.css';
+import HomeLayout from './Layout/HomeLayout';
+import Home from './pages/Homes/Home/Home';
+import Error from './Error/Error';
 
 const router = createBrowserRouter([
-   {
-      path: "/",
-      element: <Home></Home>,
+	{
+		path: '/',
+		element: <Home></Home>,
 
-      childred: [
-         {
-            path: "/",
-            element: <HomeLayout></HomeLayout>,
-         },
-      ],
-   },
+		childred: [
+			{
+				path: '/',
+				element: <HomeLayout></HomeLayout>,
+			},
+		],
+	},
+	{
+		path: '*',
+		element: <Error></Error>,
+	},
 ]);
 
-ReactDOM.createRoot(document.getElementById("root")).render(
-   <React.StrictMode>
-      <RouterProvider router={router} />
-   </React.StrictMode>
+ReactDOM.createRoot(document.getElementById('root')).render(
+	<React.StrictMode>
+		<RouterProvider router={router} />
+	</React.StrictMode>
 );
