5037106455bd2a07795e8cc55d5201790529ed38
monsur776e <monsuralam8897@gmail.com>
2023-10-11 01:16:01 +0600

    problem solved

--- src/pages/Homes/Carosel/Carosel.jsx
+++ src/pages/Homes/Carosel/Carosel.jsx
@@ -30,57 +30,57 @@ const Carosel = () => {
             className="mySwiper">
             <div className="">
                <SwiperSlide>
-                  <div className="relative">
+                  <div className="relative w-[100%]">
                      <img
                         className="w-[100%] h-[50%]  md:h-[100%] bg-cover"
                         src={img1}
                         alt=""
                      />
                      <div className="absolute top-0 left-0 bottom-0 bg-black/40 flex justify-center items-center flex-col text-center right-0 z-20">
-                        <h2 className="text-[20px] md:text-[50px] text-white font-semibold  md:font-bold mb-2 leading-none">
+                        <h2 className="text-[20px] md:text-[50px] 2xl:text-[60px] text-white font-semibold  md:font-bold mb-2 leading-none">
                            Make a difference for people
                            <br />
                            who needs it the most
                         </h2>
-                        <button className="bg-blue-500 text-[14px] md:text-[16px] uppercase mt-2 md:mt-4 px-4 md:px-6 py-2 border-none text-[#fff]">
+                        <button className="bg-blue-500 text-[14px] md:text-[18px] 2xl:text-[22px]  uppercase mt-2 md:mt-4 px-4 md:px-6 py-2 border-none text-[#fff]">
                            Start with a little
                         </button>
                      </div>
                   </div>
                </SwiperSlide>
                <SwiperSlide>
-                  <div className="relative">
+                  <div className="relative w-[100%]">
                      <img
-                        className="w-[100%] h-[50%] md:h-[100%] bg-cover"
-                        src={img2}
-                        alt=""
-                     />
-                     <div className="absolute top-0 left-0 bottom-0 bg-black/40 flex justify-center items-center flex-col text-center right-0 z-20">
-                        <h2 className="text-[20px] md:text-[50px] text-white font-semibold  md:font-bold mb-2 leading-none">
-                           Make a difference for people
-                           <br />
-                           who needs it the most
-                        </h2>
-                        <button className="bg-blue-500 text-[14px] md:text-[16px] uppercase mt-2 md:mt-4 px-4 md:px-6 py-2 border-none text-[#fff]">
-                           Start with a little
-                        </button>
-                     </div>
-                  </div>
-               </SwiperSlide>
-               <SwiperSlide>
-                  <div className="relative">
-                     <img
-                        className="w-[100%] h-[50%] md:h-[100%] bg-cover"
+                        className="w-[100%] h-[50%]  md:h-[100%] bg-cover"
                         src={img3}
                         alt=""
                      />
                      <div className="absolute top-0 left-0 bottom-0 bg-black/40 flex justify-center items-center flex-col text-center right-0 z-20">
-                        <h2 className="text-[20px] md:text-[50px] text-white font-semibold  md:font-bold mb-2 leading-none">
+                        <h2 className="text-[20px] md:text-[50px] 2xl:text-[60px] text-white font-semibold  md:font-bold mb-2 leading-none">
                            Make a difference for people
                            <br />
                            who needs it the most
                         </h2>
-                        <button className="bg-blue-500 text-[14px] md:text-[16px] uppercase mt-2 md:mt-4 px-4 md:px-6 py-2 border-none text-[#fff]">
+                        <button className="bg-blue-500 text-[14px] md:text-[18px] 2xl:text-[22px]  uppercase mt-2 md:mt-4 px-4 md:px-6 py-2 border-none text-[#fff]">
+                           Start with a little
+                        </button>
+                     </div>
+                  </div>
+               </SwiperSlide>
+               <SwiperSlide>
+                  <div className="relative w-[100%]">
+                     <img
+                        className="w-[100%] h-[50%]  md:h-[100%] bg-cover"
+                        src={img2}
+                        alt=""
+                     />
+                     <div className="absolute top-0 left-0 bottom-0 bg-black/40 flex justify-center items-center flex-col text-center right-0 z-20">
+                        <h2 className="text-[20px] md:text-[50px] 2xl:text-[60px] text-white font-semibold  md:font-bold mb-2 leading-none">
+                           Make a difference for people
+                           <br />
+                           who needs it the most
+                        </h2>
+                        <button className="bg-blue-500 text-[14px] md:text-[18px] 2xl:text-[22px]  uppercase mt-2 md:mt-4 px-4 md:px-6 py-2 border-none text-[#fff]">
                            Start with a little
                         </button>
                      </div>
